a45e304bd58379bfb36a7976204b6f4f75416fd6
haaseoo <sojun1098@naver.com>
2026-01-24 21:35:51 +0900

    잔고 조회

--- krinvestmentAPI/get_kr_stock.py
+++ krinvestmentAPI/get_kr_stock.py
@@ -4,15 +4,16 @@ import pprint
 
 with open("secrets/koreainvestment.key", "r", encoding="utf-8") as f:
     lines = f.readlines()
+
     key = lines[0].strip()
     secret = lines[1].strip()
     acc_no = lines[2].strip()
 
-broker = mojito.KoreaInvestment(
-    api_key=key,
-    api_secret=secret,
-    acc_no=acc_no,
-    mock=True
+    broker = mojito.KoreaInvestment(
+        api_key=key,
+        api_secret=secret,
+        acc_no=acc_no,
+        mock=True
 )
 
 # print(broker)
--- krinvestmentAPI/order_kr_stock.py
+++ krinvestmentAPI/order_kr_stock.py
@@ -1,42 +1,37 @@
 # 1. 잔고 조회
 import mojito
-# import pprint
+import pprint
 
-key = "key"
-secret = "secret"
-acc_no = "..-01"
-
-with open("mock.key") as f:
+with open("secrets/koreainvestment.key", "r", encoding="utf-8") as f:
     lines = f.readlines()
 
-broker = mojito.KoreaInvestment(
-    api_key=key,
-    api_secret=secret,
-    acc_no=acc_no,
-    mock=True,
+    key = lines[0].strip()
+    secret = lines[1].strip()
+    acc_no = lines[2].strip()
+
+    broker = mojito.KoreaInvestment(
+        api_key=key,
+        api_secret=secret,
+        acc_no=acc_no,
+        mock=True
 )
 
 resp = broker.fetch_balance()
 # pprint.pprint(resp)
 
-for comp in resp["output1"]:
-    print(comp['pdno'])
-    print(comp['prdt_name'])
-    print(comp['hldg_qty'])
-    print(comp['pchs_ant'])
-    print(comp['evlu_amt'])
-    print('-'*40)
+# 보유 종목 : 키 값으로 보유 종목에 대한 정보 확인
 resp = broker.fetch_balance()
-print(resp) # 서버가 보내준 전체 응답을 먼저 확인하세요!
-# 2. 매수 주문
+holdings = resp['output1']
 
+if len(holdings) == 0:
+    print("현재 보유 중인 종목이 없습니다.")
+else:
+    print(f"총 {len(holdings)}개의 종목을 보유 중입니다. \n")
 
-
-
-# 3. 매도 주문
-
-
-# 4. 주문 취소
-
-
-# 5. 주문 정정
+    for comp in resp['output1']:
+        print(comp['pdno'])
+        print(comp['prdt_name'])
+        print(comp['hldg_qty'])
+        print(comp['pchs_amt'])
+        print(comp['evlu_amt'])
+        print("-" * 20)
